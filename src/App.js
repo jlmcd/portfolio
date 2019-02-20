522ca37d5f59dfed8a9f0f09e33d7fad959cbac0
Jonathan McDonald <Jonathan@Jonathan.local>
2019-02-19 22:24:41 -0700

    landing page, skills page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
+import "./reset.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 class App extends Component {
+  state = {
+    showAbout: false,
+    showContact: false,
+    showSkills: false,
+    showProjects: false
+  };
+
+  toggleAbout = () => {
+    this.setState({
+      showAbout: !this.state.showAbout,
+      showContact: false,
+      showSkills: false,
+      showProjects: false
+    });
+  };
+  toggleContact = () => {
+    this.setState({
+      showAbout: false,
+      showContact: !this.state.showContact,
+      showSkills: false,
+      showProjects: false
+    });
+  };
+  toggleSkills = () => {
+    this.setState({
+      showAbout: false,
+      showContact: false,
+      showSkills: !this.state.showSkills,
+      showProjects: false
+    });
+  };
+  toggleProjects = () => {
+    this.setState({
+      showAbout: false,
+      showContact: false,
+      showSkills: false,
+      showProjects: !this.state.showProjects
+    });
+  };
+
   render() {
+    const { showAbout, showContact, showSkills, showProjects } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="background" />
+        <h2 onClick={this.toggleContact} className="top">Contact</h2>
+        <h2 onClick={this.toggleAbout} className="left">About</h2>
+        <h2 onClick={this.toggleSkills} className="right">Skills</h2>
+        <h2 onClick={this.toggleProjects} className="bottom">Projects</h2>
+        <div className="name">
+          <h1>Jonathan</h1>
+          <h1>___</h1>
+          <h1>McDonald</h1>
+        </div>
+        <h2 className="welcome-message">Full-stack Developer</h2>
+        <div className={showAbout ? "show" : "hide"}>
+          <About />
+        </div>
+        <div className={showContact ? "show" : "hide"}>
+          <Contact />
+        </div>
+        <div className={showSkills ? "show" : "hide"}>
+          <Skills />
+        </div>
+        <div className={showProjects ? "show" : "hide"}>
+          <Projects />
+        </div>
       </div>
     );
   }
