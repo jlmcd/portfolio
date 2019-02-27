77521bc9430469acbb212936b529430bb73f6904
Jonathan McDonald <Jonathan@Jonathan.local>
2019-02-27 14:58:36 -0700

    fixed contact page layout

---
+++ server/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const { SERVER_PORT } = process.env;
+const express = require("express");
+
+const app = express();
+app.use(express.json())
+app.use(express.static(`${__dirname}/../build`));
+
+app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`));
