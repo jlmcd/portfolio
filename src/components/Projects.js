3884beffd543139a06288af7464775566ae9b980
Jonathan McDonald <Jonathan@Jonathan.local>
2019-02-27 14:28:28 -0700

    added zooming for project pictures

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Projects.scss";
 import Slider from "react-slick";
+import ImageZoom from "react-medium-image-zoom";
 
 export default function Projects(props) {
   const carouselSettings = {
@@ -19,21 +20,39 @@ export default function Projects(props) {
       <div className="project-card">
         <Slider {...carouselSettings}>
           <div className="image">
-            <img
-              src="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=tuBmy5gAPt8vLTLrrJquu4wEvi4%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjrfMbfervz8UAffSpTjQA3eL21EjXpGo7qLontfY8ngpbtI5L5agYUbhl4lWdI"
-              alt="Ostinato Loop Editor"
+            <ImageZoom
+              image={{
+                src: "https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=tuBmy5gAPt8vLTLrrJquu4wEvi4%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjrfMbfervz8UAffSpTjQA3eL21EjXpGo7qLontfY8ngpbtI5L5agYUbhl4lWdI",
+                alt: "Ostinato Loop Editor"
+              }}
+              zoomImage={{
+                src: "https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=tuBmy5gAPt8vLTLrrJquu4wEvi4%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjrfMbfervz8UAffSpTjQA3eL21EjXpGo7qLontfY8ngpbtI5L5agYUbhl4lWdI",
+                alt: "Ostinato Loop Editor"
+              }}
             />
           </div>
           <div className="image">
-            <img
-              src="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=t1%2BVHPItfe1j0Ox0nMLMF4jD0ro%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWi6fJLXcLLzrEAUfnpTjQBgKbm1SDWzE47pK427fI532ZXiI8H5agYUbhl4lWdI"
-              alt="Ostinato Dashboard"
+            <ImageZoom
+              image={{
+                src: "https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=t1%2BVHPItfe1j0Ox0nMLMF4jD0ro%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWi6fJLXcLLzrEAUfnpTjQBgKbm1SDWzE47pK427fI532ZXiI8H5agYUbhl4lWdI",
+                alt: "Ostinato Dashboard"
+              }}
+              zoomImage={{
+                src: "https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=t1%2BVHPItfe1j0Ox0nMLMF4jD0ro%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWi6fJLXcLLzrEAUfnpTjQBgKbm1SDWzE47pK427fI532ZXiI8H5agYUbhl4lWdI",
+                alt: "Ostinato Dashboard"
+              }}
             />
           </div>
           <div className="image">
-            <img
-              src="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=0hcnKiSQk5m%2B4AP4Ri4EZ0ksZso%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjpLZHWebeookAVLHpVjQAxLL21SDnoGo7tKI26eNog35CzJ5b5agYUbhl4lWdI"
-              alt="Ostinato Account Editor"
+            <ImageZoom
+              image={{
+                src: "https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=0hcnKiSQk5m%2B4AP4Ri4EZ0ksZso%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjpLZHWebeookAVLHpVjQAxLL21SDnoGo7tKI26eNog35CzJ5b5agYUbhl4lWdI",
+                alt: "Ostinato Account Editor"
+              }}
+              zoomImage={{
+                src: "https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=0hcnKiSQk5m%2B4AP4Ri4EZ0ksZso%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjpLZHWebeookAVLHpVjQAxLL21SDnoGo7tKI26eNog35CzJ5b5agYUbhl4lWdI",
+                alt: "Ostinato Account Editor"
+              }}
             />
           </div>
         </Slider>
@@ -64,21 +83,39 @@ export default function Projects(props) {
       <div className="project-card">
         <Slider {...carouselSettings}>
           <div className="image">
-            <img
-              src="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=eUU0er4iD2R23y8Q5rCq8YXviY8%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjvfsGOeeOg8kBEfX0JjQA2Kei1EmboRo7pI9i7fdp035GxJZL5agYUbhl4lWdI"
-              alt="Cramify Game"
+          <ImageZoom
+              image={{
+                src: "https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=eUU0er4iD2R23y8Q5rCq8YXviY8%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjvfsGOeeOg8kBEfX0JjQA2Kei1EmboRo7pI9i7fdp035GxJZL5agYUbhl4lWdI",
+                alt: "Cramify Game"
+              }}
+              zoomImage={{
+                src: "https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=eUU0er4iD2R23y8Q5rCq8YXviY8%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjvfsGOeeOg8kBEfX0JjQA2Kei1EmboRo7pI9i7fdp035GxJZL5agYUbhl4lWdI",
+                alt: "Cramify Game"
+              }}
             />
           </div>
           <div className="image">
-            <img
-              src="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=IHpVkbZG5B8wZm1ZUJQGWHUDqY0%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjhfpbff-Cg8UARfi0FjQBhfui1EmWyRo67It_uftkniJDmLJL5agYUbhl4lWdI"
-              alt="Cramify Game"
+          <ImageZoom
+              image={{
+              src:"https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=IHpVkbZG5B8wZm1ZUJQGWHUDqY0%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjhfpbff-Cg8UARfi0FjQBhfui1EmWyRo67It_uftkniJDmLJL5agYUbhl4lWdI",
+              alt:"Cramify Game"
+              }}
+              zoomImage={{
+              src:"https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=IHpVkbZG5B8wZm1ZUJQGWHUDqY0%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjhfpbff-Cg8UARfi0FjQBhfui1EmWyRo67It_uftkniJDmLJL5agYUbhl4lWdI",
+              alt:"Cramify Game"
+              }}
             />
           </div>
           <div className="image">
-            <img
-              src="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=thNWC%2BpqAE3%2B5bhzHWIYzCn9YQg%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjsfM-OKrKgo0BFcXkCjQBgee-1STDjQI68K9nsftkn3pDiIZX5agYUbhl4lWdI"
-              alt="Cramify Game"
+          <ImageZoom
+              image={{
+              src:"https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=thNWC%2BpqAE3%2B5bhzHWIYzCn9YQg%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjsfM-OKrKgo0BFcXkCjQBgee-1STDjQI68K9nsftkn3pDiIZX5agYUbhl4lWdI",
+              alt:"Cramify Game"
+              }}
+              zoomImage={{
+              src:"https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=thNWC%2BpqAE3%2B5bhzHWIYzCn9YQg%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjsfM-OKrKgo0BFcXkCjQBgee-1STDjQI68K9nsftkn3pDiIZX5agYUbhl4lWdI",
+              alt:"Cramify Game"
+              }}
             />
           </div>
         </Slider>
