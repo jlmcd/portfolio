49c9a4c2b69b4ab3bfa2515bf0ae9da05b0e840e
jlmcd <jonathanlmcd@gmail.com>
2026-05-13 19:51:56 -0600

    removed unused test files, code, and packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from 'react-router-dom'
 
 const container = document.getElementById("root");
@@ -11,8 +10,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
